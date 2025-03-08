6f5b5b76ed7c18c70976a8721ed1d3d90f0bf6cd
Sharon Kendi <sharonkendi35@gmail.com>
2025-03-08 08:49:00 +0300

    Learns about strings

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// (1) Define currentUser
+let currentUser = "Sharon"; 
+
+// (2, 3, 4) Define welcomeMessage
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// (5, 6, 7) Define excitedWelcomeMessage
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// (8, 9, 10) Define shortGreeting
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
